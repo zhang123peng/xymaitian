b2241a1ee678d407c747708dbfc4f30172c09680
Bill Zhang <708947679@qq.com>
2017-08-29 10:50:27 +0800

    first commit

---
+++ data/admin/config_update.php
@@ -0,0 +1,4 @@
+<?php
+//更新服务器，如果有变动，请到 http://bbs.xymaitian.com 查询
+$updateHost = 'http://updatenew.xymaitian.com/base-v57/';
+$linkHost = 'http://flink.xymaitian.com/server_url.php';
---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = '112.74.44.200';
+$cfg_dbname = 'xymaitian.com';
+$cfg_dbuser = 'youxi';
+$cfg_dbpwd = 'youxi123';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'utf8';
+
+
+?>
